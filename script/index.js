886f2eba5d4fd04a50f62f27e182f3b2c02dcb3e
toHarsh <harshkothari17@gmail.com>
2021-11-30 21:01:14 +0530

    problem fixed

--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
 /* Set the width of the sidebar to 25rem  (show it) */
 function openNav() {
-  document.getElementById("mySidepanel").style.width = "25rem";
+  document.getElementById("mySidepanel").style.width = "30rem";
   document.getElementById("overlay").style.width = "100vw";
 }
 
